da60f31b0160ed987614b0d536c6f97cf02b9f7a
riicardods <riicardoljn@icloud.com>
2023-11-01 21:45:13 -0300

    a

--- Program.cs
+++ Program.cs
@@ -24,27 +24,27 @@
 
                     if (numero == palpite)
                     {
-                        Console.WriteLine($"VOCE ACERTOU!! O número era {numero}. Você acertou em {tentativas} tentativa(s).");
+                        Console.WriteLine($"VOCE ACERTOU!! O NÚMERO ERA {numero}. VOCÊ ACERTOU EM {tentativas} TENTATIVA(S).");
                         acertou = true;
                     }
                     else if (palpite < numero)
                     {
-                        Console.WriteLine($"ERROU!! O número é maior. Tentativas restantes: {3 - tentativas}");
+                        Console.WriteLine($"ERROU!! O NÚMERO É MAIOR. TENTATIVAS RESTANTES: {3 - tentativas}");
                     }
                     else
                     {
-                        Console.WriteLine($"ERROU!! O número é menor. Tentativas restantes: {3 - tentativas}");
+                        Console.WriteLine($"ERROU!! O NÚMERO É MENOR. TENTATIVAS RESTANTES: {3 - tentativas}");
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Por favor, digite apenas números entre 1 e 10.");
+                    Console.WriteLine("POR FAVOR, DIGITE APENAS NÚMEROS ENTRE 1 E 10.");
                 }
             }
 
             if (!acertou)
             {
-                Console.WriteLine($"Você excedeu o número máximo de tentativas. O número era {numero}.");
+                Console.WriteLine($"VOCÊ EXCEDEU O NÚMERO MÁXIMO DE TENTATIVAS. O NÚMERO ERA {numero}.");
             }
         }
     }
